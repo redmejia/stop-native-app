5595cb30b647fd2f5688c73e552024a189026a50
redmejia <preynaldorod@gmail.com>
2022-03-14 00:57:18 -0700

    Tab icons

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import {
+	Icon
+} from "react-native-elements";
+import { StyleSheet } from "react-native";
 import PantsProduct from "./PantsProduct";
 import ShirtProduct from "./ShirtProduct";
 import Home from "./Home";
@@ -9,7 +12,6 @@ import ShopProduct from "./ShopProduct";
 
 import { connect } from "react-redux";
 import { fetchProductShirts, fetchProductPants } from "../redux/ActionCreators";
-
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -28,20 +30,49 @@ const TabRoot = () => {
 
 			}}
 
-			screenOptions={{
+			
 
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({color}) => {
+					if (route.name === 'Home') {
+						return (
+							<Icon
+								name='home'
+								type='font-awesome'
+								color={color}
+							/>
+						);
+					}
+					if (route.name === 'Shirt') {
+						return (
+
+							<Icon
+								name='chevron-up'
+								type='font-awesome'
+								color='#7f8385'
+							/>
+						)
+					}
+					if (route.name === 'Pants') {
+						return (
+
+							<Icon
+								name='chevron-down'
+								type='font-awesome'
+								color='#7f8385'
+							/>
+						)
+					}
+				},
 				headerStyle: {
 					backgroundColor: '#DC3F45'
 				},
-				tabBarBadge: 3,
-				tabBarBadgeStyle: { backgroundColor: 'blue' }
-
-			}}
+			})}
 
 		>
-			<Tab.Screen name="Pants" component={PantsProduct} />
-			<Tab.Screen name="Home" component={Home} />
-			<Tab.Screen name="Shirt" component={ShirtProduct} />
+			<Tab.Screen name="Pants" component={PantsProduct} options={{tabBarLabelStyle : styles.labelStyle}} />
+			<Tab.Screen name="Home" component={Home} options={{tabBarLabelStyle : styles.labelStyle}}/>
+			<Tab.Screen name="Shirt" component={ShirtProduct} options={{tabBarLabelStyle : styles.labelStyle}}/>
 		</Tab.Navigator>
 
 	)
@@ -71,8 +102,8 @@ class Main extends Component {
 					name="Check Out"
 					component={ShopProduct}
 					options={{
-						headerStyle :{
-							backgroundColor : '#DC3F45'
+						headerStyle: {
+							backgroundColor: '#DC3F45'
 						}
 					}}
 				/>
@@ -86,5 +117,14 @@ const mapDispatchToProps = {
 	fetchProductShirts,
 	fetchProductPants
 }
-
 export default connect(null, mapDispatchToProps)(Main);
+
+const styles = StyleSheet.create({
+	iconStyle : {
+		color : '#7f8385'
+	},
+	labelStyle : {
+		fontSize : 14,
+		fontWeight : 'bold',
+	}
+})
